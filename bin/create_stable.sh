0dd516f308f9c8cb8fd1d6bc418c12fd2662bf76
Linus Gasser <linus.gasser@epfl.ch>
2017-09-07 11:11:24 +0200

    also install goimports

--- bin/create_stable.sh
+++ bin/create_stable.sh
@@ -56,14 +56,14 @@ prepare_repo(){
         show_help
     fi
 
-    REPO_DSTPATH="$GOPATH/src/$REPO_DST"
+    REPO_DST_PATH="$GOPATH/src/$REPO_DST"
 
-    if [ -e "$REPO_DSTPATH" ]; then
+    if [ -e "$REPO_DST_PATH" ]; then
         if [ "$overwrite" ]; then
-            echo "Going to overwrite '$REPO_DSTPATH'"
-            rm -rf "$REPO_DSTPATH"/*
+            echo "Going to overwrite '$REPO_DST_PATH'"
+            rm -rf "$REPO_DST_PATH"/*
         else
-            echo "'$REPO_DSTPATH' exists - either delete it or use '-o'"
+            echo "'$REPO_DST_PATH' exists - either delete it or use '-o'"
             exit 1
         fi
     fi
@@ -75,12 +75,12 @@ copy_stable(){
         exit 1
     fi
 
-    mkdir -p "$REPO_DSTPATH"
+    mkdir -p "$REPO_DST_PATH"
     local reposrc=$( cd ..; pwd )
     for d in $( cat directories ); do
         echo "Adding directory '$d' to stable"
         local dir="$reposrc/$d"
-        local repodir="$REPO_DSTPATH/$d"
+        local repodir="$REPO_DST_PATH/$d"
         mkdir -p "$repodir"
         if [ -d "$dir" ]; then
             find "$dir" -maxdepth 1 -type f | xargs -I {} cp {} "$repodir"
@@ -93,22 +93,25 @@ copy_stable(){
 
 copy_repo(){
     if [ -d overwrite ]; then
-        cp -av overwrite/* "$REPO_DSTPATH"
+        cp -av overwrite/* "$REPO_DST_PATH"
     fi
 }
 
 remove_files(){
     if [ -f remove_files ]; then
         for f in $( cat remove_files ); do
-            rm "$REPO_DSTPATH"/$f
+            rm "$REPO_DST_PATH"/$f
         done
     fi
 }
 
 update_imports(){
+	if ! which goimports 2>&1 > /dev/null; then
+		go get golang.org/x/tools/cmd/goimports
+	fi
     echo "updating imports from $REPO_SRC to $REPO_DST"
-    find "$REPO_DSTPATH" -name "*go" -exec perl -pi -e "s:$REPO_SRC:$REPO_DST:" "{}" \;
-    find "$REPO_DSTPATH" -name "*go" -exec goimports -w "{}" \;
+    find "$REPO_DST_PATH" -name "*go" -exec perl -pi -e "s:$REPO_SRC:$REPO_DST:" "{}" \;
+    find "$REPO_DST_PATH" -name "*go" -exec goimports -w "{}" \;
 }
 
 main $@
