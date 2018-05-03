5f6c3b750c385a29e62a10402263a0ac9a7553d9
Linus Gasser <linus.gasser@epfl.ch>
2018-05-03 08:24:20 +0200

    making it possible to exclude tests and be verbose when a test fails

--- bin/coveralls.sh
+++ bin/coveralls.sh
@@ -12,28 +12,30 @@ fi
 all_tests_passed=true
 
 echo "mode: atomic" > profile.cov
+tmp=$(mktemp)
 for dir in $DIR_SOURCE; do
-	if ! echo $DIR_EXCLUDE | grep -q $dir; then
-		if ls $dir/*.go >/dev/null 2>&1; then
-			if ! grep "// +build experimental" $dir/*.go > /dev/null; then
-		        go test -short -race -covermode=atomic -coverprofile=$dir/profile.tmp $dir
-
-	            if [ $? -ne 0 ]; then
-		            all_tests_passed=false
-		        fi
-	            if [ -f $dir/profile.tmp ]; then
-	                tail -n +2 $dir/profile.tmp >> profile.cov
-		            rm $dir/profile.tmp
-			    fi
-			else
-				echo -e "exp\t$dir"
-	        fi
-	    fi
+  if ! echo $DIR_EXCLUDE | grep -q $dir; then
+    if ls $dir/*.go >/dev/null 2>&1; then
+      if ! grep "// +build experimental" $dir/*.go > /dev/null; then
+        DEBUG_TIME=true go test -short -race -covermode=atomic -coverprofile=$dir/profile.tmp $dir \
+          > $tmp 2>&1
+        if [ $? -ne 0 ]; then
+          echo $tmp
+          all_tests_passed=false
+        fi
+        if [ -f $dir/profile.tmp ]; then
+          tail -n +2 $dir/profile.tmp >> profile.cov
+          rm $dir/profile.tmp
+        fi
+      else
+        echo -e "exp\t$dir"
+      fi
     fi
+  fi
 done
 
 if [ "$all_tests_passed" = true ]; then
-    exit 0
+  exit 0
 else
-    exit 1
+  exit 1
 fi
